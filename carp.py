86f614d91344b64e7472fcfc3025e9207b6ff997
Étienne Deparis <etienne@depar.is>
2016-12-13 20:32:21 +0000

    [UPD] add a GUI to carp

--- carp.py
+++ carp.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
 import carp
+import signal
+
+import gi
+gi.require_version('Gtk', '3.0')  # noqa: E402
+from gi.repository import Gtk, GLib
 
 if __name__ == "__main__":
-    carp.CarpCli()
+    if len(sys.argv) == 2 and sys.argv[1] == "gui":
+        sys.argv.pop()
+        # Install signal handlers
+        GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGTERM,
+                             Gtk.main_quit, None)
+        GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGINT,
+                             Gtk.main_quit, None)
+        carp.CarpGui()
+        Gtk.main()
+    else:
+        carp.CarpCli()
--- carp/__init__.py
+++ carp/__init__.py
@@ -1,4 +1,5 @@
 from carp.stash_manager import StashManager
 from carp.carpcli import CarpCli
+from carp.carpgui import CarpGui
 
-__all__ = ["StashManager", "CarpCli"]
+__all__ = ["StashManager", "CarpCli", "CarpGui"]
