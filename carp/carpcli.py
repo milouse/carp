af9fe70e98973af1cd44f5af88b3d3fffa95d120
Étienne Deparis <etienne@depar.is>
2017-08-01 16:21:40 +0000

    Translate help messages. Update translations and man pages

--- carp/carpcli.py
+++ carp/carpcli.py
@@ -59,67 +59,67 @@ class CarpCli:
         parser = ArgumentParser(
             description=carp_desc,
             formatter_class=RawDescriptionHelpFormatter,
-            epilog="""\
+            epilog=_("""\
 Each command has its own help. To access it, do a:
   %(prog)s command --help
 
 For exemple: %(prog)s create --help
-""")
+"""))
         parser.add_argument("-v", "--version", action="store_true",
-                            help="Display carp version information"
-                                 " and exit.")
+                            help=_("Display carp version information"
+                                   " and exit."))
         parser.add_argument("-c", "--config",
                             help=_("Customized config file."))
 
         parent_parser = ArgumentParser(add_help=False)
-        parent_parser.add_argument("stash", help="Stash to handle.")
+        parent_parser.add_argument("stash", help=_("Stash to handle."))
         parent_parser.add_argument("-t", "--test",
                                    action="store_true",
-                                   help="Dry-run.")
+                                   help=_("Dry-run."))
 
         sync_parser = ArgumentParser(add_help=False)
         sync_parser.add_argument("-n", "--nosync",
                                  action="store_true",
-                                 help="Disable lock creation/check.")
+                                 help=_("Disable lock creation/check."))
 
         subparsers = parser.add_subparsers(
-            dest="command", title="Commands", help=None,
-            metavar="what to do with your EncFS stashes:",
+            dest="command", title=_("Commands"), help=None,
+            metavar=_("what to do with your EncFS stashes:"),
             description=None)
         parser_list = subparsers.add_parser(
-            "list", help="List your EncFS stashes.")
+            "list", help=_("List your EncFS stashes."))
         parser_create = subparsers.add_parser(
-            "create", help="Create a new EncFS stash.")
+            "create", help=_("Create a new EncFS stash."))
         subparsers.add_parser(
-            "mount", help="Mount an existing EncFS stash.",
+            "mount", help=_("Mount an existing EncFS stash."),
             parents=[parent_parser, sync_parser])
         subparsers.add_parser(
-            "umount", help="Unmount a currently mounted EncFS stash.",
+            "umount", help=_("Unmount a currently mounted EncFS stash."),
             parents=[parent_parser, sync_parser])
         subparsers.add_parser(
-            "pull", help="Pull a distant stash.",
+            "pull", help=_("Pull a distant stash."),
             parents=[parent_parser])
         subparsers.add_parser(
-            "push", help="Push a distant stash.",
+            "push", help=_("Push a distant stash."),
             parents=[parent_parser])
 
         parser_create.add_argument(
             "-s", "--save-pass", action="store_true",
-            help="Save the password in a file.")
+            help=_("Save the password in a file."))
         parser_create.add_argument(
             "-m", "--mount", action="store_true",
-            help="Mount the stash after creation.")
+            help=_("Mount the stash after creation."))
         parser_create.add_argument(
-            "rootdir", help="The path to an empty folder, which will "
-            "become the encrypted stash.")
+            "rootdir", help=_("The path to an empty folder, which will "
+                              "become the encrypted stash."))
 
         parser_list.add_argument(
             "state", nargs="?", default="mounted",
             choices=["mounted", "unmounted", "all"],
-            help="What do you want to list? (default: mounted)")
+            help=_("What do you want to list? (default: mounted)"))
         parser_list.add_argument("-r", "--raw", action="store_true",
-                                 help="Don't display stash current state "
-                                 "(only useful for 'all' subcommand).")
+                                 help=_("Don't display stash current state "
+                                        "(only useful for 'all' subcommand)."))
 
         args = parser.parse_args()
 
