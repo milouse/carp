a3f6ef80b0290fd6602bf730e19920b0aa7a8ade
Étienne Deparis <etienne@depar.is>
2018-01-03 08:32:59 +0000

    Bump version

--- carp/version.py
+++ carp/version.py
@@ -1,4 +1,4 @@
 """Semantic version."""
 # To facilitate automatic extraction of the version using grep,
 # the version string should be enclosed in double quotes.
-VERSION = "0.7.1"
+VERSION = "0.7.2"
